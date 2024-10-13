ac56701fc2e2f5ed6fa076fdc82fa5068cb00e05
MargineanMiruna <miruna.marginean.2018@gmail.com>
2024-10-13 13:18:10 +0300

    dritte aufgabe 3 und 4

--- src/ArrayZahlen.java
+++ src/ArrayZahlen.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class ArrayZahlen {
 
     int[] ersteZahl, zweiteZahl;
@@ -7,6 +5,18 @@ public class ArrayZahlen {
     public ArrayZahlen(int[] ersteZahl, int[] zweiteZahl) {
         this.ersteZahl = ersteZahl;
         this.zweiteZahl = zweiteZahl;
+    }
+
+    public String toString(int[] zahl) {
+        String arrayzahl = "[";
+        int j = 0;
+        while(zahl[j] == 0)
+            j++;
+        for(int i = j; i < zahl.length; i++)
+            arrayzahl = arrayzahl + zahl[i] + " ";
+        arrayzahl += "]";
+        return arrayzahl;
+
     }
 
     public int[] Summe() {
@@ -25,22 +35,30 @@ public class ArrayZahlen {
     }
 
     public int[] Diff() {
-        if(ersteZahl[0] < zweiteZahl[0]) {
-            int[] aux = ersteZahl;
-            ersteZahl = zweiteZahl;
-            zweiteZahl = aux;
+        int[] grossereZahl, kleinereZahl;
+        int j = 0;
+
+        while(ersteZahl[j] == zweiteZahl[j])
+            j++;
+        if(ersteZahl[j] < zweiteZahl[j]) {
+            kleinereZahl = ersteZahl;
+            grossereZahl = zweiteZahl;
+        }
+        else {
+            kleinereZahl = zweiteZahl;
+            grossereZahl = ersteZahl;
         }
 
-        int[] diff = new int[ersteZahl.length];
+        int[] diff = new int[grossereZahl.length];
         int flag = 0;
 
-        for(int i = ersteZahl.length - 1; i >= 0; i--) {
-            if(ersteZahl[i] < zweiteZahl[i] + flag) {
-                diff[i] = ersteZahl[i] + 10 - zweiteZahl[i] - flag;
-                flag++;
+        for(int i = grossereZahl.length - 1; i >= 0; i--) {
+            if(grossereZahl[i] < kleinereZahl[i] + flag) {
+                diff[i] = grossereZahl[i] + 10 - kleinereZahl[i] - flag;
+                flag = 1;
             }
             else {
-                diff[i] = ersteZahl[i] - zweiteZahl[i] - flag;
+                diff[i] = grossereZahl[i] - kleinereZahl[i] - flag;
                 flag = 0;
             }
         }
@@ -48,4 +66,31 @@ public class ArrayZahlen {
         return diff;
     }
 
+    public int[] Mult(int ziffer) {
+        int[] mult = new int[ersteZahl.length + 1];
+        int flag = 0;
+
+        for(int i = ersteZahl.length; i > 0; i--) {
+            mult[i] = (ersteZahl[i - 1] * ziffer + flag) % 10;
+            flag = (ersteZahl[i - 1] * ziffer + flag) / 10;
+        }
+
+        if(flag != 0) {
+            mult[0] = flag;
+        }
+
+        return mult;
+    }
+
+    public int[] Div(int ziffer) {
+        int[] div = new int[ersteZahl.length];
+        int flag = 0;
+
+        for(int i = 0; i < ersteZahl.length; i++) {
+            div[i] = (flag  * 10 + ersteZahl[i]) / ziffer;
+            flag = (flag  * 10 + ersteZahl[i]) % ziffer;
+        }
+
+        return div;
+    }
 }
